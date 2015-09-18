bfd396bbb6a4aee4b538a095b6dfcb4c9a9fc1f3
pantelicnevena <neki_91_bg@hotmail.com>
2015-09-18 16:09:05 +0200

    Navbar (isAuthenticated)

--- client/js/controllers/MainCtrl.js
+++ client/js/controllers/MainCtrl.js
@@ -2,10 +2,13 @@
  * Created by enco on 14.9.15..
  */
 app.controller(
-  'MainCtrl', function($scope, $route, $routeParams, $location) {
+  'MainCtrl', function($scope, $route, $routeParams, $location, Account) {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
+
+    $scope.currentId = Account.getCurrentId();
+    $scope.isAuthenticated = Account.isAuthenticated();
 
     //$scope.isAuthenticated = Account.isAuthenticated();
     //$scope.currentId = Account.currentId();
