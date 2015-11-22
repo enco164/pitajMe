01cc42847fa5d00a9d960955d892dcc1cf55d131
pantelicnevena <neki_91_bg@hotmail.com>
2015-11-22 16:10:54 +0100

    Pagination

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -4,7 +4,7 @@
 
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Account', 'Post', 'Question', 'Answer', 'Comment', 'Category', 'Follow', 'Interest'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Account', 'Post', 'Question', 'Answer', 'Comment', 'Category', 'Follow', 'Interest', 'Like'];
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);
